da9301e1526f044804392fb2242c9398d54ef9c2
Arijit Chandra <arijit.chandra03@gmail.com>
2016-12-17 22:00:17 +0530

    code clean

--- server.js
+++ server.js
@@ -6,11 +6,6 @@ var port = 3000
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve(__dirname, 'client','views'))
-// app.set('views', path.resolve(__dirname,'client','views'))
-// app.set('partials', path.resolve(__dirname,'client','partials'))
-
-
-
 
 app.use('/static', express.static(path.resolve(__dirname,'client')))
 
@@ -18,20 +13,6 @@ app.use('*', function(req, res)
 {
   res.render('index.ejs')
 })
-
-// app.get('/home', function(req,res)
-// {
-//   res.render('index.ejs')
-// })
-//
-// app.get('/about', function(req,res)
-// {
-//   res.render('partials/about.html')
-// })
-// app.get('/contact', function(req,res)
-// {
-//   res.render('partials/contact.html')
-// })
 
 app.listen(port, hostname, function(){
   console.log(`Server running at http://${hostname}:${port}/`)
